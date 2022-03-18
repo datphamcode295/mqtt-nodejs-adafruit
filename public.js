779f415b016044b50a4f9b168f7534c5358f822f
datphamcode295 <vandatpro2000@gmail.com>
2022-03-18 20:50:37 +0700

    finished

--- public.js
+++ public.js
@@ -1,7 +1,8 @@
+//const client = require("./index")
 const mqtt = require('mqtt')
 
 const connectUrl = `https://io.adafruit.com:1883`
-const clientID = `subcriber` //random but unique
+const clientID = `publicer22` //random but unique
 const client = mqtt.connect(connectUrl, {
   clientID,
   clean: true,
@@ -10,24 +11,17 @@ const client = mqtt.connect(connectUrl, {
   password: 'aio_TJOF63EcVjp4oaWPmgtJisUAYMaq',//active key in adafruit io key (My Key)
   reconnectPeriod: 1000,
 })
-
 const topic = 'vandat2000/feeds/temp'
 
-//subscribe to the topic
-client.on('connect', () => {
-  console.log('Connected')
-  client.subscribe([topic], () => {
-    console.log(`Subscribe to topic '${topic}'`)
-    client.on('message', (topic, payload) => {
-        console.log('Received Message:', topic, payload.toString())
-      })
-  })
+    client.publish(topic, '50', { qos: 0, retain: false }, (error) => {
+      if (error) {
+        console.error(error)
+      }
+      else{
+        client.end()
 
-
-  
-})
-
-
+      }
+    })
 
 
 
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Node.js Adafruit MQTT Client Examples
+
+For more documentation on the use of the Javascript MQTT.js client, see the [Javascript Client - documentation](https://github.com/mqttjs/MQTT.js)
+
+## Installation
+
+* Install Node.js and NPM
+
+* Installation dependencies
+
+  ```bash
+  npm install
+  // or use yarn
+  yarn
+  ```
+
+* Start
+    Replace your username and key in the code
+
+    ```bash
+    npm index.js
+    //to run subscribe
+    npm public.js
+    //to public data to the topic
+    ```
+
